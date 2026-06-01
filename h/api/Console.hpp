94652f267ff679c80b789bcfb2ad688a4ed9aa43
marko <maremare005@gmail.com>
2026-06-01 16:26:39 +0200

    c++ console wrapper added

---
+++ h/api/Console.hpp
@@ -0,0 +1,16 @@
+#ifndef CONSOLE_HPP
+#define CONSOLE_HPP
+
+#include "../syscall_c.h"
+
+class Console {
+  public:
+    static char getc();
+    static void putc(char);
+};
+
+inline char Console::getc() { return getc(); }
+
+inline void Console::putc(char c) { putc(c); }
+
+#endif
--- h/syscall_cpp.hpp
+++ h/syscall_cpp.hpp
@@ -1,17 +1,12 @@
 #ifndef SYSCALL_CPP_HPP
 #define SYSCALL_CPP_HPP
 
+#include "api/Console.hpp"
 #include "api/PeriodicThread.hpp"
 #include "api/Semaphore.hpp"
 #include "api/Thread.hpp"
 
 void *operator new(size_t);
 void operator delete(void *);
-
-class Console {
-  public:
-    static char getc();
-    static void putc(char);
-};
 
 #endif
